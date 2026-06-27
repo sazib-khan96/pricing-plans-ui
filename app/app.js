39b70191bce7ae440ca65a6d8ed713c94331b894
sazib-khan96 <info.sazibkhan@gmail.com>
2026-06-27 14:02:01 -0700

    Add dark and light mode toggle

--- app/app.js
+++ app/app.js
@@ -1,30 +1,39 @@
+const togggleBtn = document.getElementById("checkbox");
+const monthText = document.querySelector(".monthly_text");
+const yearText = document.querySelector(".yearly_text");
 
+const darkBtn = document.getElementById("dark-Btn");
+const cards = document.querySelectorAll(".card");
 
-const togggleBtn = document.getElementById('checkbox');
-const monthText = document.querySelector(".monthly_text")
-const yearText = document.querySelector(".yearly_text")
-console.log
+darkBtn.addEventListener("click", () => {
+  document.body.classList.toggle("active");
+  cards.forEach((card) => {
+    card.classList.toggle("active");
+  });
+  if ((document.body.classList.contains = "active")) {
+    darkBtn.innerText = "Light Mood";
+  } else {
+    darkBtnText.innerText = "Dark mood";
+    //  cards.classList.remove('active')
+  }
+});
 
-function containerToggle (id){
-    const container = document.querySelectorAll('.container');
-    for(let cont of container){
-      cont.style.display = "none"
-    }
-    document.getElementById(id).style.display = "flex";
+function containerToggle(id) {
+  const container = document.querySelectorAll(".container");
+  for (let cont of container) {
+    cont.style.display = "none";
+  }
+  document.getElementById(id).style.display = "flex";
 }
 
-
-togggleBtn.addEventListener("change",()=>{
-
-   if(togggleBtn.checked){
-     containerToggle('yearly')
-     yearText.classList.add("active")
-     monthText.classList.add('active')
-   }
-   else{
-    containerToggle("monthly")
-    monthText.classList.remove('active')
-    yearText.classList.remove("active")
-   }
-    
-})
+togggleBtn.addEventListener("change", () => {
+  if (togggleBtn.checked) {
+    containerToggle("yearly");
+    yearText.classList.add("active");
+    monthText.classList.add("active");
+  } else {
+    containerToggle("monthly");
+    monthText.classList.remove("active");
+    yearText.classList.remove("active");
+  }
+});
